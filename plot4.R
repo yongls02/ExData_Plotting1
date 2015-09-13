28faf782d502f34c69ed269745e0d00048f4680b
yongls02 <yongls02@hotmail.com>
2015-09-13 17:53:13 +0800

    Hello2

--- plot4.R
+++ plot4.R
@@ -39,12 +39,5 @@ legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=
 
 plot(data$datetime, data$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
 
-################
-
-##plot(data$datetime, data$Sub_metering_1, type="l", main="", xlab="",
-##     ylab="Energy sub metering" )
-##lines(data$datetime, data$Sub_metering_2, type="l", col="red")
-##lines(data$datetime, data$Sub_metering_3, type="l", col="blue")
-##legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 
 dev.off()
